4b171d5ec8085c5bdad1d92c7641fd84331a81ca
Chris Rune <gtrabbit@gmail.com>
2017-09-24 22:31:05 -0400

    feat: Convert static HTML to React App
    Making it so books display dynamically from API and as React
    components rather than as static HTML.

---
+++ src/bookdisplay.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class BookDisplay extends Component {
+	static PropTypes = {
+		book: PropTypes.object.isRequired,
+		onMoveBook: PropTypes.func.isRequired
+
+
+	}
+
+	state = {}
+
+	moveBook = (shelf) => {
+		this.props.onMoveBook(this.props.book.shelf, shelf, this.props.book.id)
+	}
+	
+
+
+	render() {
+
+		return (
+		<li>
+			<div className="book">
+				<div className="book-top">
+					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+					<div className="book-shelf-changer">
+						<select
+							onChange={(event)=> this.moveBook(event.target.value)}
+						>
+							<option value="none" disabled>Move to...</option>
+							<option value="currentlyReading">Currently Reading</option>
+							<option value="wantToRead">Want to Read</option>
+							<option value="read">Read</option>
+							<option value="none">None</option>
+						</select>
+					</div>
+				</div>
+			<div className="book-title">{this.props.book.title}</div>
+			<div className="book-authors">
+				{this.props.book.authors.map(a=>(
+					<p key={a}> {a} </p>
+
+					))}
+			</div>
+			</div>
+		</li>)
+	}
+
+
+}
+
+export default BookDisplay;
---
+++ src/booklist.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BookDisplay from './bookdisplay'
+
+
+class BookList extends Component {
+	static PropTypes = {
+		books: PropTypes.array.isRequired,
+		shelf: PropTypes.object.isRequired,
+		onMoveBook: PropTypes.func.isRequired
+
+	}
+
+	state = {}
+
+	render() {
+
+		return (
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{this.props.shelf.title}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                    	{this.props.books.map((a,i)=>(
+                    		<BookDisplay 
+                    			book={a}
+                    			key={i}
+                    			onMoveBook={this.props.onMoveBook} />
+
+                    		))}
+                    </ol>
+                  </div>
+                </div>
+                
+        )}
+
+}
+
+export default BookList;
